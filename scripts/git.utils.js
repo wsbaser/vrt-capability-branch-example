8118e80697a707f9986e07714119b715662febbb
Aleksey Volkov <aleksey_volkov@epam.com>
2023-06-29 14:14:26 +0200

    add logging for git commands

--- scripts/git.utils.js
+++ scripts/git.utils.js
@@ -1,31 +1,36 @@
 const {execSync} = require('child_process')
 
+const exec = (command)=>{
+    console.log(command)
+    return execSync(command).toString().trim()
+}
+
 module.exports.getCurrentBranch = ()=>{
-    return execSync('git rev-parse --abbrev-ref HEAD').toString().trim()
+    return exec('git rev-parse --abbrev-ref HEAD').toString().trim()
 }
 
 module.exports.clearStage=()=>{
-    return execSync('git reset .').toString().trim()
+    return exec('git reset .').toString().trim()
 }
 
 module.exports.createCommit = (message)=>{
-    return execSync(`git commit --allow-empty -m'${message}'`).toString().trim()
+    return exec(`git commit --allow-empty -m'${message}'`).toString().trim()
 }
 
 module.exports.push = (name)=>{
     if(name){
-        execSync(`git push origin ${name}`)
+        exec(`git push origin ${name}`)
     }else{
-        execSync(`git push`)
+        exec(`git push`)
     }
 }
 
 module.exports.addTag = (name)=>{
-    return execSync(`git tag ${name}`)
+    return exec(`git tag ${name}`)
 }
 
 module.exports.findTag=(pattern)=>{
-    const lines = execSync('git log --format="%D"').toString().split('\n')
+    const lines = exec('git log --format="%D"').toString().split('\n')
     const tags = lines.map(l=>l.split(',')
             .filter(ref=>ref.trim()
             .indexOf('tag:')!=-1)
